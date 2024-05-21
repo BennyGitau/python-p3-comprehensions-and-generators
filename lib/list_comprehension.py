26f2279957227f770877a96af7cf8ed5f6ff89c2
BensonGitau <bensonkamaugitau@gmail.com>
2024-05-21 16:34:51 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
-
+num_list = range(10)
 def return_evens(num_list):
-    pass
 
+    evens_list = [n for n in num_list if n%2 == 0]
+    print(evens_list)
+    return evens_list
+return_evens(num_list)
+
+sentence_list = [
+        "I like computers",
+        "I require coffee",
+        "Live long and prosper",
+]
 def make_exclamation(sentence_list):
-    pass
+    sentences_with_exclamation = [s if s.endswith('!') else s + '!' for s in sentence_list]
+    print(sentences_with_exclamation)
+    return sentences_with_exclamation
+
+make_exclamation(sentence_list)
